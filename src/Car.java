eb801f5a7c8e2496f69030688969a84c17043f94
noa.strandberg <noa.strandberg@gmail.com>
2024-04-10 14:41:22 +0200

    måndag 8/4

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,19 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
 public class Car {
-}
+    int engineRating;
+    int aerodynamicRating;
+    int suspensionRating;
+    int durabilityRating;
+    int carStats[] = new int[3];
+
+    public Car(int engineStats,int aerodynamicStats, int suspensionStats, int durabilityStats){
+        carStats[0] = engineStats;
+        carStats[1] = aerodynamicStats;
+        carStats[2] = suspensionStats;
+        carStats[3] = durabilityStats;
+
+        }
+    }
+
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
 public class Main {
+
+    static void randomizerMethod(){
+        for (int i = 0; i < 3; i++) {
+            double randNr = Math.random() * 100;
+            int randomRating = (int) randNr;
+            System.out.println(randNr);
+        }
+    }
     public static void main(String[] args) {
 
         System.out.println("F1 Manager!");
+        randomizerMethod();
+
+        Car MaxVerstapebil = new Car(100,99,70, 90);
+        Car SophiaFluschr = new Car(15,50, 69, 80);
+
+        //Randomizes the rating for the car's attributes from 0-99
+
+
     }
 }
